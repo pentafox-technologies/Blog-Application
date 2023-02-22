395203a83c27637d6819f6c716bd5e5d342667b4
anish-rm <anishmahi946@gmail.com>
2023-02-22 19:09:32 +0530

    create update completed

--- server/controllers/articleController.js
+++ server/controllers/articleController.js
@@ -10,9 +10,25 @@ exports.getAllArticle = (req, res, next) => {
 exports.createArticle = async (req, res) => {
     const client = await db.connect();
     try{                                       
-        console.log(req.body);
-        const categories = req.body.category.split(" ");     
+        // splitting categories to array
+        const categories = req.body.category.split(" ");
+        
+        // here we are inserting into categorySet Table
+        // And we will check whether that category does not exist, if true we will insert it by categorized under = "Other"
+        for(let i=0;i<categories.length;i++){
+            const created =new Date();
+            let checkcategory = await client.query(`select * from "CategorySet" where "catName" like '${categories[i]}'`);
+            if(checkcategory.rows.length == 0){
+                let categorizedUnder = "Other"
+                let newCategory = await client.query(`insert into "CategorySet" ("catName", "categorizedUnder", "initializedBy", "dateCreated") values($1,$2,$3,$4) RETURNING *`, [categories[i],categorizedUnder,req.user.userName,created ]);
+            }
+        }
+
+        // Initially we are creating slug based on title
         let slug = slugify(req.body.title, { lower: true }) + Math.random().toString(36).slice(2);
+        
+        // Checking whether sulg already exists
+        // running this loop until we get unique slug
         while(true){
             temp = await client.query(`select * from "Article" where slug like '${slug}'`);
             if(temp.rows.length>0){
@@ -22,21 +38,27 @@ exports.createArticle = async (req, res) => {
                 break;
             }
         }
+
+        // Initially when creating status will always be draft
         const status = "draft";
+
+        // Visibilty will be initially private
         const visibilty = "private";
-        // const newArticle = await client.query(`insert into "Article" ("slug", "author","title","content","status","visibility") values($1,$2,$3,$4,$5,$6) RETURNING *`, [slug,req.body.author,req.body.title, req.body.content, status, visibilty]);
+
+        //  Query for creating article
+        const newArticle = await client.query(`insert into "Article" ("slug", "author","title","content","status","visibility") values($1,$2,$3,$4,$5,$6) RETURNING *`, [slug,req.user.userName,req.body.title, req.body.content, status, visibilty]);
+        
+        // we have to insert the category and article into categoryMap.
         for(let i=0;i<categories.length;i++){
-            const created =new Date();
-            let checkcategory = await client.query(`select * from "CategorySet" where "catName" like '${categories[i]}'`);
-            if(checkcategory.rows.length == 0){
-                let categorizedUnder = "Other"
-                let newCategory = await client.query(`insert into "CategorySet" ("catName", "categorizedUnder", "initializedBy", "dateCreated") values($1,$2,$3,$4) RETURNING *`, [categories[i],categorizedUnder,req.user.userName,created ]);
-            }
+            const categoryMap = await client.query(`insert into "CategoryMap" ("article", "category") values($1,$2) RETURNING *`, [newArticle.rows[0].slug,categories[i]]);
         }
+
+        // sending response finally ☺️
         res.status(201).json({
             status: 'success',
-            // data: newArticle
+            data: newArticle
         });
+
     } catch(err){
         console.log(err);
         res.status(400).json({
@@ -55,10 +77,80 @@ exports.getArticle = (req, res, next) => {
     });
 };
 
-exports.updateArticle = (req, res, next) => {
-    res.status(200).json({
-        status: 'success'
-    });
+exports.updateArticle = async (req, res) => {
+
+
+ const client = await db.connect();
+    try{                                       
+        // splitting categories to array
+        let categories = [];
+        if(req.body.category){
+            categories =  req.body.category.split(" ");
+        }
+        console.log(categories);
+        // here we are inserting into categorySet Table
+        // And we will check whether that category does not exist, if true we will insert it by categorized under = "Other"
+        if(categories.length>0){
+            for(let i=0;i<categories.length;i++){
+                const created =new Date();
+                let checkcategory = await client.query(`select * from "CategorySet" where "catName" like '${categories[i]}'`);
+                if(checkcategory.rows.length == 0){
+                    let categorizedUnder = "Other"
+                    let newCategory = await client.query(`insert into "CategorySet" ("catName", "categorizedUnder", "initializedBy", "dateCreated") values($1,$2,$3,$4) RETURNING *`, [categories[i],categorizedUnder,req.user.userName,created ]);
+                }
+            }
+        }
+        
+        var query = ['UPDATE "Article"'];
+        query.push('SET');
+
+        // Create another array storing each set command
+        // and assigning a number value for parameterized query
+        let set = [];
+        console.log(req.body);
+        let values = [];
+        Object.keys(req.body).forEach(function (key, i) {
+            if(key!='category'){
+                set.push(key + ' = ($' + (i + 1) + ')'); 
+                values.push(req.body[key]);
+            } 
+        });
+        query.push(set.join(', '));
+
+
+        // // Add the WHERE statement to look up by id
+        query.push('WHERE "slug" = ' + `'${req.params.slug}' RETURNING *` );
+
+        // // Return a complete query string
+        query = query.join(' ')
+        console.log(query);
+
+
+        //  Query for creating article
+        const Article = await client.query(query, values);
+        
+        // // we have to insert the category and article into categoryMap.
+        // select * from "CategorySet" where "catName" like '${categories[i]}'
+        for(let i=0;i<categories.length;i++){
+            const iscategory = await client.query(`SELECT * FROM "CategoryMap" WHERE "category" = ($1) and "article"= ($2)`, [categories[i], req.params.slug]);
+            if(iscategory.rows.length == 0){
+                const categoryMap = await client.query(`insert into "CategoryMap" ("article", "category") values($1,$2) RETURNING *`, [req.params.slug,categories[i]]);
+            }
+        }
+
+        // sending response finally ☺️
+        res.status(200).json({
+            status: 'success',
+            data: Article
+        });
+
+    } catch(err){
+        console.log(err);
+        res.status(400).json({
+            status:'error',
+            message: err
+        });
+    }
 };
 
 exports.deleteArticle = (req, res, next) => {
