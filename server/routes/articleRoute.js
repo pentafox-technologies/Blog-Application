0367ed01e6edd163f00ce92e02780efd6bd76c88
RANJTH KUMAR <90784111+RANJITHKUMAR-E@users.noreply.github.com>
2023-03-05 17:17:16 +0530

    Request validation done in art

--- server/routes/articleRoute.js
+++ server/routes/articleRoute.js
@@ -8,6 +8,7 @@ router.route('/').get(articleController.getAllArticle).post(authController.prote
 
 router.route('/:slug').get(articleController.getArticle).patch(authController.protect, articleController.updateArticle).delete(articleController.deleteArticle);
 
+router.route('/validation/:slug').get(authController.protect, articleController.validateRequest);
 
 router.get("/create", (req, res) =>
 {
